02eed5c4dee89da933f28f954290c699c8331a04
rhafi <excyuef@gmail.com>
2025-11-18 17:43:44 +0700

    change products array to class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummaryHTML } from "./checkout/orderSummary.js";
 import { renderOrderPaymentHTML } from "./checkout/orderPayment.js";
-import '../data/cart-class.js'
+// import '../data/cart-class.js'
 
 renderOrderSummaryHTML();
 renderOrderPaymentHTML();
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -43,7 +43,7 @@ export function renderOrderSummaryHTML () {
               ${matchingProduct.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
+              ${matchingProduct.getPrice()}
             </div>
             <div class="product-quantity">
               <span>
