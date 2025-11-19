f8c165673d9746835c0025851f4b89325b2330cf
rhafi <excyuef@gmail.com>
2025-11-19 21:55:02 +0700

    use inheritance, and use the teknik polymorphism and method overriding

--- data/products.js
+++ data/products.js
@@ -22,6 +22,29 @@ class Product {
   getPrice () {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML () {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor (productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML () {
+    // super.extraInfoHTML()
+    return `
+      <a 
+      href="${this.sizeChartLink}" 
+      target="_blank"> 
+        Size Chart 
+      </a>`;
+  }
 }
 
 export function getProduct (productId) {
@@ -696,5 +719,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing (productDetails);
+  }
+
   return new Product (productDetails);
 });
