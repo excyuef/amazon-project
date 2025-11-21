4f4311755ef6b581c85bd5c8f6db5ef5fdf05ca3
rhafi <excyuef@gmail.com>
2025-11-21 21:59:34 +0700

    backend practice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummaryHTML } from "./checkout/orderSummary.js";
 import { renderOrderPaymentHTML } from "./checkout/orderPayment.js";
 // import '../data/cart-class.js'
+import '../data/backend-practice.js';
 
 renderOrderSummaryHTML();
 renderOrderPaymentHTML();
