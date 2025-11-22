84c4f29e885abd394e3fa3aba970537f55b97661
rhafi <excyuef@gmail.com>
2025-11-23 01:02:31 +0700

    learn promises

--- data/products.js
+++ data/products.js
@@ -100,7 +100,7 @@ export function loadProducts (fun) {
             return new Product (productDetails);
           }
     });
-    console.log(products);
+    console.log('loaded');
     fun();
   });
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,58 @@
 import { renderOrderSummaryHTML } from "./checkout/orderSummary.js";
 import { renderOrderPaymentHTML } from "./checkout/orderPayment.js";
 import { loadProducts } from "../data/products.js";
+import {loadCart} from '../data/cart.js'
+
+Promise.all([
+  new Promise((resolve) => {
+    console.log('memulai promises');
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+
+  new Promise ((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+
+]).then(() => {
+  renderOrderSummaryHTML();
+  renderOrderPaymentHTML();
+});
+
+
+/*
+Promises - not promise.all
+
+new Promise((resolve) => {
+  console.log('memulai promises');
+  loadProducts(() => {
+    resolve('menyelesaikan loadProducts');
+  });
+
+}).then((hasil) => {
+  console.log(hasil);
+
+  return new Promise ((resolve) => {
+    loadCart(() => {
+      resolve('menyelesaikan loadCart');
+    });
+  });
+
+}).then((hasil2) => {
+  console.log(hasil2);
+  renderOrderSummaryHTML();
+  renderOrderPaymentHTML();
+});
+
+
+
+Nested - Callbacks
 
 loadProducts(() => {
   renderOrderSummaryHTML();
   renderOrderPaymentHTML();
 });
+*/
